eea9c463b3834aa8f6eff92d5ef61079fdd46a91
Vinoth Rajendran <r.vinoth@live.com>
2019-12-01 21:14:00 +0530

    Component Dialog sample added

---
+++ 13ComponentDialog/UserDialog/rootdialog/RootDialog.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UserDialog.rootdialog
+{
+    public class RootDialog<T> : ActivityHandler where T : Dialog
+    {
+        BotState convState;
+        BotState userState;
+        Dialog dlgRoot;
+        public RootDialog(ConversationState conversation,UserState userstate, T Dialog)
+        {
+            convState = conversation;
+            userState = userstate;
+            dlgRoot = Dialog;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            await convState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await userState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        protected async override Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await dlgRoot.RunAsync(turnContext, convState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+        }
+    }
+}
---
+++ 13ComponentDialog/UserDialog/subdialog/UserDialog.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UserDialog.subdialog
+{
+    public class UserDialog : ComponentDialog
+    {
+        public UserDialog() : base(nameof(UserDialog))
+        {
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new NumberPrompt<int>("NumberPrompt"));
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                GetUserName,
+                GetMobileNo,
+                ConformDlg
+            }));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> ConformDlg(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["Phone"] = stepContext.Result;
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> GetMobileNo(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["UserName"] = stepContext.Result;
+
+            var promptUser = stepContext.PromptAsync("NumberPrompt", new PromptOptions()
+            {
+                Prompt = MessageFactory.Text("Please enter your phone Number"),
+                RetryPrompt = MessageFactory.Text("Hello , enter the valid number")
+            }, cancellationToken);
+
+            return await promptUser;
+        }
+
+        private async Task<DialogTurnResult> GetUserName(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var promptUser = stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions()
+            {
+                Prompt = MessageFactory.Text($"Please enter your name"),
+
+            }, cancellationToken);
+
+            return await promptUser;
+        }
+    }
+}
