5ebc2388613121277307d02c31e7c29917a3efc9
Vinoth Rajendran <r.vinoth@live.com>
2019-12-02 06:11:41 +0530

    Middlware added

---
+++ 14Middleware/SampleMiddleware/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleMiddleware.Middleware
+{
+    public class LoggerMiddleware : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
+        {
+            //logger 
+            await turnContext.SendActivityAsync("Hey am Logger Middleware");
+            await next(cancellationToken);
+        }
+    }
+
+    public class GrammerMiddleware : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
+        {
+            await turnContext.SendActivityAsync("Hey am Grammer Middleware");
+            await next(cancellationToken);
+        }
+    }
+}
