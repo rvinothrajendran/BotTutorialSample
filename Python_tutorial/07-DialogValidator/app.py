c5d794d18ffa4d9e6d31875bcd9abab711d42e30
Vinoth Rajendran <r.vinoth@live.com>
2019-12-26 16:50:58 +0530

    Add Custom validation

---
+++ Python_tutorial/07-DialogValidator/app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,Response
+from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings,TurnContext,ConversationState,MemoryStorage
+from botbuilder.schema import Activity
+import asyncio
+from botdialog import BotDialog
+
+app = Flask(__name__)
+loop = asyncio.get_event_loop()
+
+botsettings = BotFrameworkAdapterSettings("","")
+botadapter = BotFrameworkAdapter(botsettings)
+
+CONMEMORY = ConversationState(MemoryStorage())
+botdialog = BotDialog(CONMEMORY)
+
+
+@app.route("/api/messages",methods=["POST"])
+def messages():
+    if "application/json" in request.headers["content-type"]:
+        body = request.json
+    else:
+        return Response(status = 415)
+
+    activity = Activity().deserialize(body)
+
+    auth_header = (request.headers["Authorization"] if "Authorization" in request.headers else "")
+
+    async def call_fun(turncontext):
+        await botdialog.on_turn(turncontext)
+
+    task = loop.create_task(
+        botadapter.process_activity(activity,auth_header,call_fun)
+        )
+    loop.run_until_complete(task)
+
+
+if __name__ == '__main__':
+    app.run('localhost',3978)
