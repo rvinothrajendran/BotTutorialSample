fe2fa5cb1bf38343ad79a6c0ce7b8240fc509f7b
Vinoth <r.vinoth@live.com>
2020-05-30 07:18:13 +0530

    commit "Move csharp samples to csharp folder"

---
+++ csharp_tutorial/13ComponentDialog/UserDialog/subdialog/maindialog.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UserDialog.subdialog
+{
+    public class maindialog : ComponentDialog
+    {
+        public maindialog(): base(nameof(maindialog))
+        {
+            AddDialog(new MovieListDialog());
+            AddDialog(new UserDialog());
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                StartMovieDlg,
+                StartUserDlg,
+                ShowResult
+            }));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }        
+
+        private async Task<DialogTurnResult> StartMovieDlg(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.BeginDialogAsync(nameof(MovieListDialog), null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> StartUserDlg(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.BeginDialogAsync(nameof(UserDialog), stepContext.Result, cancellationToken);           
+        }
+        private async Task<DialogTurnResult> ShowResult(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.EndDialogAsync(null, cancellationToken);           
+        }
+    }
+}
---
+++ csharp_tutorial/LuisSample/LuisHelper/LuisHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Configuration;
+
+namespace LuisSample.LuisHelper
+{
+    public class LuisHelper : IRecognizer
+    {
+        private readonly LuisRecognizer _luisRecognizer;
+
+        public LuisHelper()
+        {
+
+            var service = new LuisService()
+            {
+                AppId = "",
+                SubscriptionKey = "",
+                Region = "westus",
+                Version = "2.0"
+            };
+
+            var app = new LuisApplication(service);
+            var regOptions = new LuisRecognizerOptionsV2(app)
+            {
+                IncludeAPIResults = true,
+                PredictionOptions = new LuisPredictionOptions()
+                {
+                    IncludeAllIntents = true,
+                    IncludeInstanceData = true
+                }
+            };
+
+            _luisRecognizer = new LuisRecognizer(regOptions);
+
+        }
+
+        public async Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            return await _luisRecognizer.RecognizeAsync(turnContext, cancellationToken);
+        }
+
+        public async Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken) where T : IRecognizerConvert, new()
+        {
+            return await _luisRecognizer.RecognizeAsync<T>(turnContext, cancellationToken);
+        }
+    }
+}
