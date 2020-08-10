a76972202ec20096670265cd823addd3cfc72a19
rvinothrajendran <r.vinoth@live.com>
2020-08-10 09:07:58 +0530

    Added Adaptive dialog basis sample

---
+++ AdaptiveDialog/Csharp/AdaptiveCard/AdaptiveCard1/BotDialog/RootDialog.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers;
+
+namespace AdaptiveCard1.BotDialog
+{
+    public class RootDialog : ComponentDialog
+    {
+        public RootDialog() : base(nameof(RootDialog))
+        {
+           // var templates = Templates.ParseFile(Path.Combine(".", "Dialogs", "RootDialog.lg"));
+
+            var rootDialog = new AdaptiveDialog
+            {
+                Recognizer = CreateRecognizer(),
+                Triggers = new List<OnCondition>
+                {
+                    new OnIntent()
+                    {
+                        Intent = "book",
+                        Actions = new List<Dialog>() {new SendActivity("Hey am Book Actions")}
+                    },
+
+                    new OnUnknownIntent()
+                    {
+                        Actions = new List<Dialog>() {new SendActivity("Sorry , not understand")}
+                    }
+
+
+                }
+            };
+
+            AddDialog(rootDialog);
+
+            InitialDialogId = nameof(AdaptiveDialog);
+        }
+
+
+        private Recognizer CreateRecognizer()
+        {
+            var reg = new RegexRecognizer
+            {
+                Intents = new List<IntentPattern>
+                {
+                    new IntentPattern("book", "(?i)book"),
+                    new IntentPattern("weather", "(?i)weather")
+                }
+            };
+            return reg;
+        }
+    }
+}
---
+++ AdaptiveDialog/Csharp/AdaptiveCard/AdaptiveCard1/Startup.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.9.2
+
+using AdaptiveCard1.BotDialog;
+using AdaptiveCard1.Helper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AdaptiveCard1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddControllers().AddNewtonsoftJson();
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+            services.AddSingleton<UserState>();
+            services.AddSingleton<ConversationState>();
+            services.AddSingleton<RootDialog>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddSingleton<IBot, DialogBot<RootDialog>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            // app.UseHttpsRedirection();
+        }
+    }
+}
