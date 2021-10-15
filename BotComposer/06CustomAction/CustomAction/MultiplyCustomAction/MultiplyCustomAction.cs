e7d48b9266c75235b0fe6017a511fff7d4c83e65
Vinoth Rajendran <r.vinoth@live.com>
2021-10-15 16:15:46 +0200

    Added custom action for composer (#11)
    * Added Luis code
    
    * Added .gitignore information
    
    * 05AllowInterruptions Sample code
    
    * Added custom action

---
+++ BotComposer/06CustomAction/CustomAction/MultiplyCustomAction/MultiplyCustomAction.cs
@@ -0,0 +1,49 @@
+﻿using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiplyCustomAction
+{
+    //Multiply of two numbers
+    public class MultiplyCustomAction : Dialog
+    {
+        public MultiplyCustomAction([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0) : base()
+        {
+            RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        [JsonProperty("$Kind")]
+        public const string Kind = "MultiplyCustomAction";
+
+        [JsonProperty("Number1")]
+        public NumberExpression Number1 { get; set; }
+
+        [JsonProperty("Number2")]
+        public NumberExpression Number2 {  get; set; }  
+
+
+        [JsonProperty("resultProperty")]
+        public StringExpression ResultProperty { get; set; }
+
+        public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+
+            var num1 = Number1.GetValue(dc.State);
+            var num2 = Number2.GetValue(dc.State);
+
+            var result = Convert.ToInt32(num1) * Convert.ToInt32(num2) ;
+
+            if (ResultProperty != null)
+            {
+                dc.State.SetValue(this.ResultProperty.GetValue(dc.State), result);
+            }
+
+            return dc.EndDialogAsync(result: result, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ BotComposer/06CustomAction/CustomAction/MultiplyCustomAction/MultiplyCustomActionBotComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiplyCustomAction
+{
+    public class MultiplyCustomActionBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<DeclarativeType>(sp =>
+                new DeclarativeType<MultiplyCustomAction>(MultiplyCustomAction.Kind));
+        }
+    }
+}
